1b24d4a7b8c4b6f802672a28ced0363f34d2b641
ubermanu <manu@paca.happy-dev.fr>
2023-10-02 11:44:47 +0200

    do not import the whole pkg json

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,11 @@
 import sade from 'sade'
-import pkg from '../package.json' assert { type: 'json' }
+import { version } from '../package.json'
 import { bobun } from './bobun'
 
 const program = sade('bobun', true)
 
 program
-  .version(pkg.version)
+  .version(version)
   .describe('Build your frontend assets with Bobun.')
   .option('--minify', 'Minify the output', false)
   .option('--sourcemap', 'Generate sourcemaps', false)
